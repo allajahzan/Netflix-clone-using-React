73fe9441f4880c3ac9aa5bb569d60f2bfc5fe999
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-09-17 20:50:03 +0530

    updated firebase config

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { addDoc, collection, getDocs, getFirestore, query, where } from "firebase/firestore";
 import { toast } from "react-toastify";
 import Cookies from "js-cookie";
